8025849e835ca92e4140caf6eb9bce762bfc59a9
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-04-18 22:38:42 +0700

    feat: product list in home page

---
+++ apps/nextjs/src/features/home/components/ProductList.tsx
@@ -0,0 +1,120 @@
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import {
+  Box,
+  Grid,
+  GridItem,
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  SimpleGrid,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import {
+  IoChevronBackOutline,
+  IoChevronForwardOutline,
+  IoSearch,
+} from "react-icons/io5";
+import { useDebounce } from "use-debounce";
+
+import { api } from "~/utils/api";
+import { ProductCard } from "./ProductCard";
+
+export const ProductList = () => {
+  const [page, setPage] = useState<number>(1);
+  const [searchProduct, setSearchProduct] = useState<string>("");
+  const [debouncedSearchProduct] = useDebounce(searchProduct, 1000);
+
+  const {
+    data: products,
+    isLoading,
+    isRefetching,
+  } = api.product.getProducts.useQuery(
+    {
+      limit: 2,
+      page,
+      name: debouncedSearchProduct,
+    },
+    {
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  const router = useRouter();
+
+  const renderProducts = () => {
+    return products?.results.map(({ id, image, name, price, rating }) => {
+      return (
+        <ProductCard
+          key={id}
+          image={image}
+          productName={name}
+          price={price}
+          rating={rating}
+        />
+      );
+    });
+  };
+
+  const onSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchProduct(e?.currentTarget.value);
+  };
+
+  useEffect(() => {
+    void router.push({
+      query: {
+        page,
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  return (
+    <Box pb="8">
+      <InputGroup>
+        <Input mb="4" placeholder="Cari produk..." onChange={onSearchChange} />
+        <InputRightElement pointerEvents="none">
+          <Icon as={IoSearch} />
+        </InputRightElement>
+      </InputGroup>
+      {isLoading || isRefetching ? (
+        <HStack justifyContent="center">
+          <Spinner />
+        </HStack>
+      ) : (
+        <SimpleGrid columns={2} spacing={4} rowGap={4}>
+          {renderProducts()}
+        </SimpleGrid>
+      )}
+      <Grid templateColumns="repeat(3, 1fr)" mt="8">
+        <GridItem display="flex" justifyContent="start" alignItems="center">
+          {products?.meta.page !== 1 && (
+            <IconButton
+              aria-label="Previous page"
+              icon={<Icon as={IoChevronBackOutline} />}
+              onClick={() => setPage((prevPage) => prevPage - 1)}
+            />
+          )}
+        </GridItem>
+        <GridItem display="flex" justifyContent="center" alignItems="center">
+          <Text fontSize="lg" fontWeight="bold">
+            {products?.meta.page}
+          </Text>
+        </GridItem>
+        <GridItem display="flex" justifyContent="end" alignItems="center">
+          {products?.meta.hasNext && (
+            <IconButton
+              aria-label="Previous page"
+              icon={<Icon as={IoChevronForwardOutline} />}
+              onClick={() => setPage((prevPage) => prevPage + 1)}
+            />
+          )}
+        </GridItem>
+      </Grid>
+    </Box>
+  );
+};
